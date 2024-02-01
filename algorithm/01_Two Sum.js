0b7c2379f5bb30a3b9728163ed685cb886680490
Wilbur <744534984cwl@gmail.com>
2024-02-01 10:00:03 +0800

    chore: algorithm

--- algorithm/01_Two Sum.js
+++ algorithm/01_Two Sum.js
@@ -1,17 +1,18 @@
 const nums = [2, 7, 11, 15];
 const target = 9;
 
+// 2 + ? = 9
+
 function solution(nums, target) {
-  const map = new Map();
-  for(let i=0; i<nums.length; i++) {
-    const complement = target - nums[i]
-    if(map.has(complement)) {
-      return [i, map.get(complement)]
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    const implement = target - nums[i];
+    if(map.has(implement)) {
+      return [i, map.get(implement)]
     } else {
-       map.set(nums[i], i)
+      map.set(nums[i], i)
     }
   }
-  
 }
 
 console.log(solution(nums, target));
---
+++ algorithm/02_selection sort.js
@@ -0,0 +1,7 @@
+const arr = [2, 4, 3, 6, 5, 1];
+
+function selctionSort(nums) {
+  
+}
+
+console.log(selctionSort(arr))
